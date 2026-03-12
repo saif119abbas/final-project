195737e2bca263f3442cff3c96ed8c01243a76c6
SaifAdawla Abbas <saifaldawlaabbas@gmail.com>
2026-03-12 11:34:25 +0200

    feat(add-cd):add-cd-workflow

---
+++ dist/core/models/pipeline.model.js
@@ -0,0 +1,7 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+export const pipelines = pgTable("pipelines", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull(),
+    description: text("description"),
+    created_at: timestamp("created_at").defaultNow(),
+});
