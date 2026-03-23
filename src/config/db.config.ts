4a60e4fcfbd62dcb672f0d501dc1c31165924113
SaifAdawla Abbas <saifaldawlaabbas@gmail.com>
2026-03-23 23:34:27 +0200

    feat(resolve-db-confif)

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,17 +1,31 @@
 ﻿import type { MigrationConfig } from "drizzle-orm/migrator";
 
 const url = process.env.DATABASE_URL || process.env.DB_URL || "";
-//const apiKey=process.env.POLKA_KEY || ""
+const isCloudRun = !!process.env.CLOUD_SQL_CONNECTION_NAME;
+const dbName = process.env.DB_NAME ?? "zapier_clone";
+const dbPassword = process.env.DB_PASSWORD!;
+const connectionName = process.env.CLOUD_SQL_CONNECTION_NAME!;
+const dbUser = process.env.DB_USER ?? "postgres";
 const migrationConfig: MigrationConfig = {
   migrationsFolder: "./src/infrastructure/db/migrations",
 };
 type DBConfig = {
   migrationConfig: MigrationConfig;
   url: string;
+  isCloudRun: boolean;
+  dbName: string;
+  dbPassword: string;
+  connectionName: string;
+  dbUser: string;
 };
 const config: DBConfig = {
   migrationConfig,
   url,
+  isCloudRun,
+  dbName,
+  dbPassword,
+  connectionName,
+  dbUser,
 };
 
 export default config;
